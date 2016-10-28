7da4c58a9def6c04ebc21903b0f7454d5d71824b
stripathy <sujit.tripathy@icloud.com>
2016-10-27 17:00:39 -0700

    First Commit

---
+++ src/main/java/com/hyperion/metadata/DocMetadataRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.hyperion.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocMetadataRepositoryApplication {
+
+	public static void main(String[] args) {
+			SpringApplication.run(DocMetadataRepositoryApplication.class, args);
+	}
+}
